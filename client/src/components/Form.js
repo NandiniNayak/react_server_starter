32ee00f0e192f3f7214de01abeea121a9010638c
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-10-30 20:24:06 +1100

    listing blogs

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,81 +2,87 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Form extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    // console.log(this);
     this.state = {
       title: "",
       description: ""
     };
   }
 
-  logChange = event => {
-    this.setState({
-      // the title and description from the form which corresponds to the attribute name is passed as an event target
-      [event.target.name]: event.target.value
-    });
-    console.log(this.state);
-  };
-  // every event method must be a arrow method
-  handleSubmit = event => {
-    event.preventDefault();
-    // create a blog data that can be posted to the express route
-    // create a copy of the state variable
-    var newBlog = {
+  handleSubmit = e => {
+    e.preventDefault();
+    // create a variable that should be passed to the database
+    var blog = {
       title: this.state.title,
       description: this.state.description
     };
-    // to make a post request to the express route
+    console.log("blog data is fetched");
+    console.log(blog);
     axios
-      .post("/api/blogs", newBlog)
+      .post("/api/blogs", blog)
       .then(response => {
-        console.log(response);
-        console.log("data saved successfully");
+        console.log(response.data);
+        console.log("saved successfully");
+        this.props.handleNewBlog(response.data);
       })
       .catch(err => console.log(err));
   };
+  logChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <div>
-        <h1> Add Blog</h1>
-        <div className="row">
-          <form onSubmit={this.handleSubmit} className="col s12" method="post">
+      <div className="container">
+        <h1> Add a Blog </h1>
+        <div className="card">
+          <div className="card-content">
             <div className="row">
-              <div className="input-field col s6">
-                <input
-                  placeholder="Title"
-                  id="title"
-                  type="text"
-                  className="validate"
-                  name="title"
-                  value={this.state.title}
-                  onChange={this.logChange}
-                />
-              </div>
+              <form onSubmit={this.handleSubmit} method="post">
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input
+                      placeholder="Title"
+                      id="first_name"
+                      type="text"
+                      className="validate"
+                      name="title"
+                      value={this.state.title}
+                      onChange={this.logChange}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s6">
+                    {/*<label for="description">Description</label>*/}
+                    <input
+                      placeholder="Description"
+                      id="first_name"
+                      type="text"
+                      className="validate"
+                      name="description"
+                      value={this.state.description}
+                      onChange={this.logChange}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s6">
+                    <button
+                      className="btn waves-effect waves-light"
+                      type="submit"
+                      name="action"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="row">
-              <div className="input-field col s6">
-                <input
-                  id="description"
-                  type="text"
-                  className="validate"
-                  name="description"
-                  value={this.state.description}
-                  onChange={this.logChange}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <button
-                className="btn waves-effect waves-light"
-                type="submit"
-                name="action"
-              >
-                Submit
-                <i className="material-icons right" />
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     );
