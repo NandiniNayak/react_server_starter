905e3e6d473f5a4cfc5e4c71dbc8bbf4b31df923
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-11-07 13:39:23 +1100

    Added build script for client build on production

--- app.js
+++ app.js
@@ -65,8 +65,26 @@ app.use((req, res, next) => {
 // use auth Routes : anything that routes to /auth goes to auth.js
 app.use("/auth", auth);
 app.use("/api/blogs", blog);
+
+// run only in production : deploy to heroku with react client
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  //like our main.js, or main.css file
+  //this checks the routes hosted in express server
+  // any get request comes in for any route on our application and if we do not have route handler set up look up for the client side implementation
+  app.use(express.static('client/build'))
+
+  //this checks for the routes not in express server but defined only on client side like a static route
+  //Express will serve up the index.html file
+  //if it doesn't recognize the route
+  // return index.html if route was not found in server or client/build
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
+  })
+
 var port = process.env.PORT || 5000;
-// start the server and loisten on the port
+// start the server and listen on the port
 app.listen(port, () => {
   // res.send('HELLO');
   console.log(`started server on port ${port}`);
